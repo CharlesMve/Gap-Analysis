4c9c129fa55f2f7924923d3f79db376596937484
charlesmve <mvondo.mve@gmail.com>
2025-05-22 13:17:53 -0400

    added html and css and updated the parser

--- pdfparser.py
+++ pdfparser.py
@@ -1,8 +1,45 @@
 import pymupdf4llm
 import pymupdf
+from dotenv import load_dotenv
 from llama_index.core import Document
+from google import genai
+import requests
+import os
+import tkinter as tk
+from tkinter import filedialog
 
-md_read = pymupdf4llm.LlamaMarkdownReader()
-data = md_read.load_data("C:\\Users\\20029822\\Documents\\Gap Analysis\\Navistar DR100159-06.pdf")
+load_dotenv()
 
-print(data[29].get_content())
+client = genai.Client(api_key=os.getenv("API_KEY"))
+
+def get_LLM_response(prompt):
+    pass
+
+def get_PDF_file():
+# Prompt user to select a PDF file
+    root = tk.Tk()
+    root.withdraw()
+    pdf_path = filedialog.askopenfilename(
+        title="Select PDF file",
+        filetypes=[("PDF files", "*.pdf")]
+    )
+    if not pdf_path:
+        print("No file selected.")
+        exit()
+    return pdf_path
+
+
+if __name__ == "__main__":
+    # Prompt user to select a PDF file
+    example_pdf = get_PDF_file
+
+    print('\n***Example output for the LLM***\n')
+    # Load the PDF file
+    pdf_reader = pymupdf4llm.LlamaPDFReader()
+    data = pdf_reader.load_data(example_pdf)
+
+    # Print the content of the 30th page
+    if len(data) >= 30:
+        print(data[29].get_content())
+    else:
+        print("The selected PDF has less than 30 pages.")
