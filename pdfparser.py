47fe5e21d7a3d99856ce749fa9348b38730f95f5
charlesmve <mvondo.mve@gmail.com>
2025-05-22 14:36:57 -0400

    updated pdfparser; added to the terminal version

--- pdfparser.py
+++ pdfparser.py
@@ -1,19 +1,23 @@
+import os
+import requests
 import pymupdf4llm
-import pymupdf
 from dotenv import load_dotenv
 from llama_index.core import Document
+from llama_index.llms.google_genai import GoogleGenAI
+from llama_index.core.llms import ChatMessage
 from google import genai
-import requests
-import os
 import tkinter as tk
 from tkinter import filedialog
 
 load_dotenv()
 
-client = genai.Client(api_key=os.getenv("API_KEY"))
 
 def get_LLM_response(prompt):
-    pass
+    llm = GoogleGenAI(
+        model="gemini-2.0-flash",
+        api_key=os.getenv("API_KEY"),
+    )
+
 
 def get_PDF_file():
 # Prompt user to select a PDF file
@@ -30,16 +34,21 @@ def get_PDF_file():
 
 
 if __name__ == "__main__":
-    # Prompt user to select a PDF file
-    example_pdf = get_PDF_file
+    print("Select OE Document: ")
+    OE_PDF = get_PDF_file()
+    print(OE_PDF,"\n")
 
-    print('\n***Example output for the LLM***\n')
-    # Load the PDF file
+    print("Select Bendix Document: ")
+    Bendix_PDF = get_PDF_file()
+    print(Bendix_PDF,"\n")
+
+    #Load and parse the PDF files
     pdf_reader = pymupdf4llm.LlamaPDFReader()
-    data = pdf_reader.load_data(example_pdf)
+    OE_data = pdf_reader.load_data(OE_PDF)
+    Bendix_data = pdf_reader.load_data(Bendix_PDF)
 
-    # Print the content of the 30th page
+    #Print the content of the 30th page
     if len(data) >= 30:
         print(data[29].get_content())
     else:
-        print("The selected PDF has less than 30 pages.")
+       print("The selected PDF has less than 30 pages.")
