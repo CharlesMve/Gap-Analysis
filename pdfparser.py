ec9a5880e9c6295834efc9960d8967a54976586b
charlesmve <mvondo.mve@gmail.com>
2025-05-22 09:16:29 -0400

    added starter code to build on

--- pdfparser.py
+++ pdfparser.py
@@ -1,3 +1,8 @@
 import pymupdf4llm
 import pymupdf
 from llama_index.core import Document
+
+md_read = pymupdf4llm.LlamaMarkdownReader()
+data = md_read.load_data("C:\\Users\\20029822\\Documents\\Gap Analysis\\Navistar DR100159-06.pdf")
+
+print(data[29].get_content())
